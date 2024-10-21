197768b973497e420e760cff5b6e21430c229c7a
Ihedioha Chidozie <Ihediohachidozie@gmail.com>
2024-10-20 23:30:10 -0400

    changes to settings

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     #     "PORT": os.environ.get("DB_PORT"),
     # },
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         "NAME": os.environ.get("DB_NAME"),
         "USER": os.environ.get("DB_USER"),
         "PASSWORD": os.environ.get("DB_PASSWORD"),
